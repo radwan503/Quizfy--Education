d7ae284aaa185e4e7118a0847b9bb60ef78f0743
RadwanDev <radwan.ahmed@tigerit.com>
2020-06-26 19:33:32 +0600

    all page

--- js/main.js
+++ js/main.js
@@ -1,7 +1,17 @@
 (function ($) {
 "use strict";
 
-
+$('.counter-count').each(function () {
+    $(this).prop('Counter',0).animate({
+        Counter: $(this).text()
+    }, {
+        duration: 5000,
+        easing: 'swing',
+        step: function (now) {
+            $(this).text(Math.ceil(now));
+        }
+    });
+});
 
 
 
